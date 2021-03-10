59ed47021805508a43d3c0ee6157ceacec500f5f
Amin-Hashemian2021 <78362239+Amin-Hashemian2021@users.noreply.github.com>
2021-03-11 00:47:43 +0300

    Create AminPage.js

---
+++ src/components/interns/AminPage.js
@@ -0,0 +1,18 @@
+
+ 
+import React , {Component} from 'react'
+
+
+class Amin extends Component {
+     
+  render () {
+    return (
+    
+       <div>
+       <h1>the new page will be uploaded on 12.03 there is a change</h1>
+        </div>
+    )  
+  }
+}
+
+export default Amin;
