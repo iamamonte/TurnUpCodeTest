16576c9cfcb16e5519c0d5f442cfcac5d395ede6
Amin-Hashemian2021 <78362239+Amin-Hashemian2021@users.noreply.github.com>
2021-03-15 19:19:51 +0300

    Create Task_One.js

---
+++ src/components/interns/AminTasks/Task_One.js
@@ -0,0 +1,36 @@
+
+import React, { Component } from 'react';
+import {  Button, Container } from 'react-bootstrap';
+import { increment, decrement } from '../../../actions/index';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+
+    class Counter extends Component{
+      render(){
+        console.log(this.props.counter);
+        return(
+          
+            
+            <Container>
+                <h1>{this.props.counter}</h1>
+                <button onClick= {() => this.props.increment()}>
+                <h1>Increment</h1>
+                </button>
+                <button onClick= {() => this.props.decrement()}>
+                <h1>Decrement</h1>
+                </button>
+            </Container>
+        );
+      }
+    }
+    function mapStateToProps(state){
+      return{
+        counter : state.counter
+      };
+    }
+    function matchDispatchToProps(dispatch){
+      return bindActionCreators({increment: increment, decrement: decrement}, dispatch)
+    }
+
+
+    export default connect(mapStateToProps, matchDispatchToProps)(Counter);
